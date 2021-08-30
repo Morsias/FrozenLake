515f0abdb5ef48b898d5ca6486ce7c6c19c3fff8
GravIntra <lgravias@intracom-telecom.com>
2021-08-30 18:30:40 +0300

    - Made train and inference into functions - Added DQN capabilities - Fixed checkpoint and results saving

--- test_model.py
+++ test_model.py
@@ -1,8 +1,57 @@
 import argparse
 import ray
 import ray.rllib.agents.ppo as ppo
+import ray.rllib.agents.dqn as dqn
 import gym
 import pandas as pd
+from datetime import datetime
+
+def test_model(checkpoint_path: str, episodes: int, save_results: bool):
+    """
+    This is the main testing function for the FrozenLake8x8 environment. It loads a PPO or DQN agent and runs an
+    inference process. The results can be saved in .csv.
+    Args:
+        checkpoint_path (str): The path of the saved RL agent that the user wants to restore. The path must end on
+        a folder that includes a .tune_metadata file
+        episodes (int): Number of inference episodes done by the agent
+        save_results (bool): Whether the training results should be saved in a csv file for further analysis
+
+    Returns:
+
+    """
+    exp_time = "{:%Y_%m_%d_%H_%M_%S}".format(datetime.now())
+    ray.init()
+    if "PPO" in checkpoint_path:
+        config = ppo.DEFAULT_CONFIG.copy()
+        agent = ppo.PPOTrainer(config=config, env="FrozenLake8x8-v1")
+    elif "DQN" in checkpoint_path:
+        config = dqn.DEFAULT_CONFIG.copy()
+        agent = dqn.DQNTrainer(config=config, env="FrozenLake8x8-v1")
+    else:
+        raise Exception("The given checkpoint is not currently supported. Please load a DQN or PPO checkpoint")
+
+    agent.restore(checkpoint_path)
+
+
+    env = gym.make("FrozenLake8x8-v1")
+    results = []
+    for i in range(episodes):
+
+        episode_reward = 0
+        observation = env.reset()
+        done = False
+        while not done:
+            if args.render:
+                env.render()
+            action = agent.compute_single_action(observation, explore=False)
+            observation, reward, done, info = env.step(action)
+            episode_reward += reward
+        results.append(episode_reward)
+
+
+    if save_results:
+        results_df = pd.DataFrame(results, columns=["EpisodeReward"])
+        results_df.to_csv("results/inference_results_%s.csv" % exp_time)
 
 
 parser = argparse.ArgumentParser()
@@ -12,10 +61,10 @@ parser.add_argument(
     default="models/PPO_FrozenLake8x8-v1_2021-08-29_12-44-17hwu4pe41/checkpoint_000210/checkpoint-210",
     help="RL model that will be restored")
 parser.add_argument(
-    "--iters",
+    "--episodes",
     type=int,
     default=1000,
-    help="Number of iterations of inference.")
+    help="Number of episodes of inference")
 parser.add_argument(
     "--render",
     type=bool,
@@ -30,30 +79,6 @@ parser.add_argument(
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
-    ray.init()
-    config = ppo.DEFAULT_CONFIG.copy()
-    agent = ppo.PPOTrainer(config=config, env="FrozenLake8x8-v1")
-
-    agent.restore(args.checkpoint_path)
+    test_model(args.checkpoint_path, args.episodes, args.save_results)
 
 
-    env = gym.make("FrozenLake8x8-v1")
-    results = []
-    for i in range(args.iters):
-
-        episode_reward = 0
-        observation = env.reset()
-        done = False
-        while not done:
-            if args.render:
-                env.render()
-            action = agent.compute_single_action(observation, explore=False)
-            observation, reward, done, info = env.step(action)
-            episode_reward += reward
-        results.append(episode_reward)
-
-
-    if args.save_results:
-        results_df = pd.DataFrame(results, columns=["EpisodeReward"])
-        results_df.to_csv("results/inference_results.csv")
--- train.py
+++ train.py
@@ -1,20 +1,79 @@
 import argparse
 import ray
 import ray.rllib.agents.ppo as ppo
-from ray.tune.logger import pretty_print
-import time
+import ray.rllib.agents.dqn as dqn
+from datetime import datetime
 import pandas as pd
+
+
+def train_agent(iterations: int, save_results: bool, method: str):
+    """
+    This is the main training function for the FrozenLake8x8 environment. It trains a PPO or DQN agent and saves the
+    checkpoints of the models every 50 iterations. Additionally, the mean reward per iteration is kept
+    Args:
+        iterations (int): Number of training iterations done by the agent. Each iteration number a larger number of
+        timesteps. E.g. PPO runs 4000 steps per iteration
+        save_results (bool): Whether the training results should be saved in a csv file for further analysis
+        method (str): Rl methodology used. The two options currently are "PPO" and "DQN"
+
+    Returns:
+
+    """
+    exp_time = "{:%Y_%m_%d_%H_%M_%S}".format(datetime.now())
+    ray.init()
+    if method == "PPO":
+        config = ppo.DEFAULT_CONFIG.copy()
+        # These create multiple parallel environments which massively speeds up training
+        # the values when chosen after some experimentation in my own laptop
+        config["num_workers"] = 3
+        config['num_envs_per_worker'] = 8
+        trainer = ppo.PPOTrainer(config=config, env="FrozenLake8x8-v1")
+    elif method == "DQN":
+        config = dqn.DEFAULT_CONFIG.copy()
+        config["num_workers"] = 3
+        config['num_envs_per_worker'] = 8
+        config['timesteps_per_iteration'] = 4000
+        trainer = dqn.DQNTrainer(config=config, env="FrozenLake8x8-v1")
+    else:
+        raise Exception("The method provided is not supported please choose PPO or DQN")
+
+
+
+
+    mean_reward_results = []
+    for i in range(iterations):
+        # Perform one iteration of training the policy
+        result = trainer.train()
+        print("episode_reward_max: %s" % (result['episode_reward_max']))
+        print("episode_reward_mean: %s" % (result['episode_reward_mean']))
+        print("episodes_this_iter:%s" % (result['episodes_this_iter']))
+        print("time_this_iter_s: %s" % (result['time_this_iter_s']))
+        mean_reward_results.append(result['episode_reward_mean'])
+
+        # Create a model checkpoint every 50 training iterations
+        if i % 50 == 0:
+            checkpoint = trainer.save(
+                checkpoint_dir="models/%s_%s" % (method, exp_time))
+            print("checkpoint saved at", checkpoint)
+
+
+    if save_results:
+        results_df = pd.DataFrame(mean_reward_results, columns=["MeanEpisodeReward"])
+        results_df.to_csv("results/training_results_%s.csv" % exp_time)
+
+    print("Total training time: %s secs" % result['time_total_s'])
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--method",
     type=str,
-    default="PPO",
+    default="DQN",
     help="RL Methodology used. Can run with PPO and DQN")
 parser.add_argument(
     "--iters",
     type=int,
-    default=200,
+    default=5,
     help="Number of iterations to train.")
 parser.add_argument(
     "--save-results",
@@ -24,34 +83,5 @@ parser.add_argument(
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    start_time = time.time()
-
-    ray.init()
-    config = ppo.DEFAULT_CONFIG.copy()
-    config["num_gpus"] = 0
-    config["num_workers"] = 3
-    config['num_envs_per_worker'] = 8
-
-    trainer = ppo.PPOTrainer(config=config, env="FrozenLake8x8-v1")
-    mean_reward_results = []
-    for i in range(args.iters):
-        # Perform one iteration of training the policy
-        result = trainer.train()
-        print("episode_reward_max:" + result['episode_reward_max'])
-        print("episode_reward_mean:" + result['episode_reward_mean'])
-        print("episodes_this_iter:" + result['episodes_this_iter'])
-        print("time_this_iter_s:" + result['time_this_iter_s'])
-        mean_reward_results.append(result['episode_reward_mean'])
-
-        # Create a model checkpoint every 50 training iterations
-        if i % 50 == 0:
-            checkpoint = trainer.save()
-            print("checkpoint saved at", checkpoint)
-
-        print("--- The training took %s seconds ---" % (time.time() - start_time))
-
-
-    if args.save_results:
-        results_df = pd.DataFrame(mean_reward_results, columns=["MeanEpisodeReward"])
-        results_df.to_csv("results/training_results.csv")
-    ray.shutdown()
+    train_agent(args.iters, args.save_results, args.method)
+    # ray.shutdown()
